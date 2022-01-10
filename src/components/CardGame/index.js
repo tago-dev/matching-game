fcbdc8293b886bf74bf052f27951375e6aee688c
Bettega <tiagobettega.tb@gmail.com>
2022-01-10 15:01:04 -0300

    :tada: first commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame() {
+    return `
+        <article class="card-game">
+          <img src="images/alura-pixel.svg" alt="logo da alura"/>
+        </article>
+    `;
+}
+
+export default CardGame;
