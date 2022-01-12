8e90a4357baf5efef1886662c6badd79e3bd0558
Bettega <tiagobettega.tb@gmail.com>
2022-01-12 16:19:03 -0300

    :lipstick: Area do Board Game

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ import CardGame from "./src/components/CardGame";
 import BoardGame from "./src/objects/BoardGame";
 
 const $root = document.querySelector('#root');
-const $htmlCardGame = BoardGame(3);
+const $htmlCardGame = BoardGame(6);
 
 $root.insertAdjacentHTML("beforeend", $htmlCardGame);
